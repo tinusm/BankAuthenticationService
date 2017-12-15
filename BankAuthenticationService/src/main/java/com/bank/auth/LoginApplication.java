2a91e5095e4677c15f8cee513175ae326d3e84fc
smtinu <tinusm1980@gmail.com>
2017-12-15 10:35:42 +0530

    test3

--- BankAuthenticationService/src/main/java/com/bank/auth/LoginApplication.java
+++ BankAuthenticationService/src/main/java/com/bank/auth/LoginApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class LoginApplication {
 
 	public static void main(String[] args) {
